49c5f55c0326ea178c7bf470222fbe7f324593da
Oluwasegun Olaiya <oluwaseguno@ng.neolife.com>
2020-08-20 19:31:50 +0100

    Configuring github actions

--- app/Controllers/HomeController.js
+++ app/Controllers/HomeController.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 router.get('/', (req, res) => {
 
-    return res.json({ status: true, message: 'softcom.stackoverflow.com is running as expected', data: null })
+    return res.json({ status: true, message: 'softcom.stackoverflow.com is running as expected', data: {documentation: 'https://documenter.getpostman.com/view/1050902/T1LTejEJ'} })
 })
 
 module.exports = router
